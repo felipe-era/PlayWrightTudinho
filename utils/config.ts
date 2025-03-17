864d56c01b99df6061fec1fd4dd00ea3d2c37c40
feveloper <felip@FEVOLEPER>
2025-03-17 09:32:19 -0300

    First

---
+++ utils/config.ts
@@ -0,0 +1,15 @@
+
+//URLs de desenvolvimento e homologação
+const devConfig = {
+    baseUrl: '',
+    loginUrl: ''
+  };
+  
+  const homologConfig = {
+    baseUrl: 'xxxxxxx' 
+  };
+  
+  //devConfig' ou 'homologConfig' conforme necessário
+  const activeConfig = devConfig;
+    export default activeConfig;
+  
